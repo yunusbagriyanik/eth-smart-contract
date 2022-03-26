e568cc4aa82f6346d1a3a4dcf4c5f338227a9c57
yunusbagriyanik <yunusbgrynk@gmail.com>
2022-03-27 02:17:56 +0300

    migrating task smart contract to blockchain

--- contracts/TaskList.sol
+++ contracts/TaskList.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TaskList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    doTask("Task created.");
+  }
+
+  function doTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
+
 }
