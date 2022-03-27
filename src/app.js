c24ebb4eeefe2c3daf20829cc9bb5b2df795360c
yunusbagriyanik <yunusbgrynk@gmail.com>
2022-03-28 01:53:31 +0300

    load contract

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 App = {
+  loading: false,
+  contracts: {},
 
   load: async () => {
     await App.loadWeb3()
     await App.loadAccount()
+    await App.loadContract()
+    await App.render()
   },
 
 
   loadWeb3: async () => {
-
+    /*
     function one(el, type, fn) {
       function handler(event) {
         el.removeEventListener(type, handler);
@@ -19,8 +23,7 @@ App = {
     one(window, 'click', function() {
       swal("Hello!", "Welcome to the Web3 World!", "success");
     });
-
-
+    */
     if (web3.__isMetaMaskShim__) {
       console.log("Connected Metamask...")
     }
@@ -62,6 +65,42 @@ App = {
     App.account = web3.eth.accounts[0]
     console.log(App.account)
   },
+
+  loadContract: async () => {
+    const taskList = await $.getJSON('TaskList.json')
+    console.log(taskList.networks)
+
+    App.contracts.TaskList = TruffleContract(taskList)
+    App.contracts.TaskList.setProvider(App.web3Provider)
+    console.log(taskList)
+
+    App.taskList = await App.contracts.TaskList.deployed()
+  },
+
+  render: async () => {
+    if (App.loading) {
+      return
+    }
+
+    App.setLoading(true)
+
+    $('#account').html(App.account)
+
+    App.setLoading(false)
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean
+    const loader = $('#loader')
+    const content = $('#content')
+    if (boolean) {
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
+  }
 
 }
 
